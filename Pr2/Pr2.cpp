2e9a4a9f0b0bb5305c6e6110fad884350f24c8be
Vitaliy5555-sp <vitalik280508@gmail,com>
2025-09-20 14:09:26 +0300

    Pr2

---
+++ Pr2/Pr2.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include <cmath> 
+
+using namespace std;
+
+int main()
+{
+    double x;
+    double y;
+    cout << "Enter x: ";
+    cin >> x;
+    cout << "Enter y: ";
+    cin >> y;
+
+    double z1;
+    double z2;
+    double cosx = cos(x);
+    double siny = sin(y);
+    double sin2x = sin(2 * x);
+
+    z1 = (cosx * cosx * cosx * cosx) + (siny * siny) + 1.0 / 4 * (sin2x * sin2x) - 1;
+    z2 = sin(y + x) * sin(y - x);
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+    return 0;
+}
